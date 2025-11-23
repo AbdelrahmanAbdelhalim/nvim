6204890b54fdf27adee2f5e69b1ba39cfdea0aab
Abdelrahman Abdelhalim <abdelrahmanbahaaeldin@gmail.com>
2025-11-23 20:03:14 +0200

    added sql formatter and obsidian plugin

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -5,9 +5,19 @@ return {
 			lua = { "stylua" },
 			rust = { "rustfmt" },
 			python = { "isort", "black" },
-			javascript = { { "prettierd", "prettier" } },
+			javascript = { "prettierd", "prettier" },
 			tex = { "latexindent" },
 			html = { "prettier" },
+			sql = { "sqlfluff_mysql" },
+		},
+		formatters = {
+			sqlfluff_mysql = {
+				command = "sqlfluff",
+				args = { "fix", "--dialect", "mysql", "-" },
+				stdin = true,
+				cwd = nil,
+				require_cwd = false,
+			},
 		},
 		format_on_save = {
 			timeout_ms = 500,
